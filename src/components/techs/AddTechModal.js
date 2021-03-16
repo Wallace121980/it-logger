b59101a267be5b3c14af74825d4c57249068e228
Wallace121980 <williamschrauwen@gmail.com>
2021-03-16 20:05:06 +0100

    Implemented Add Technicians

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
@@ -12,7 +15,12 @@ const AddTechModal = () => {
         classes: 'rounded red',
       });
     } else {
-      console.log(firstName, lastName);
+      addTech({ firstName, lastName });
+
+      M.toast({
+        html: `${firstName} ${lastName} was added as a tech`,
+        classes: 'rounded green',
+      });
 
       // Clear Fields
       setFirstName('');
@@ -66,4 +74,8 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
